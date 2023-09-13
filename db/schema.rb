b31df79c1151a06d59647468f41033eb92c5cccf
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-09-13 13:39:42 +0000

    Add

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_09_09_074707) do
   create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
-    t.integer "user"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
