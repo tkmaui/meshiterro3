faad5ed51366c3358ad498c1707cf351d3560f65
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-09-15 06:22:53 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:edit, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
