ee8e9153bb0f28eafe679443ce2e15333fe4fb8c
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-09-15 04:39:15 +0000

    Add

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,7 +6,7 @@ class PostImagesController < ApplicationController
   def index
     @post_images = PostImage.all
   end
-  
+
   def show
     @post_image = PostImage.find(params[:id])
   end
@@ -15,6 +15,12 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
+    redirect_to post_images_path
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
     redirect_to post_images_path
   end
 
