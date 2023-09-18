fa495d93d167f91844009f76708a4c4993dc557c
tkmaui <129600101+tkmaui@users.noreply.github.com>
2023-09-18 13:47:43 +0000

    Add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user.id)
   end
 
-
   private
 
   def user_params
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,9 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   belongs_to :user
+  
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
   def favorited_by?(user)
     favorites.exists?(user_id: user_id)
